64db384303d0b566547317270c779462c2f6d85a
alvintan05 <alvintandiardi@gmail.com>
2018-04-06 16:19:14 +0700

    commit pertama

---
+++ app/src/main/java/com/example/angga/b_sport/PilihLapang.java
@@ -0,0 +1,13 @@
+package com.example.angga.b_sport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PilihLapang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_lapang);
+    }
+}
